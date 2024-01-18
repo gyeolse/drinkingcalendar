78498f1976ebfdb866dcce36dc1f019e2779bcec
gyeolse <threewave@kakao.com>
2024-01-19 01:39:39 +0900

    [ENH] : Make month header bar - make month header bar and month navigation bar - reference : calendar library (kizitonwose)

--- app/src/main/java/com/gyeolse/drinkingcalendar/MainActivity.kt
+++ app/src/main/java/com/gyeolse/drinkingcalendar/MainActivity.kt
@@ -21,11 +21,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,6 +36,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.gyeolse.drinkingcalendar.ui.theme.DrinkingcalendarTheme
+import com.kizitonwose.calendar.compose.CalendarLayoutInfo
+import com.kizitonwose.calendar.compose.CalendarState
 import com.kizitonwose.calendar.compose.HorizontalCalendar
 import com.kizitonwose.calendar.compose.VerticalCalendar
 import com.kizitonwose.calendar.compose.rememberCalendarState
@@ -42,6 +47,10 @@ import com.kizitonwose.calendar.core.CalendarMonth
 import com.kizitonwose.calendar.core.DayPosition
 import com.kizitonwose.calendar.core.daysOfWeek
 import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
+import com.kizitonwose.calendar.core.nextMonth
+import com.kizitonwose.calendar.core.previousMonth
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.launch
 import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.YearMonth
@@ -69,11 +78,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     val currentMonth = remember { YearMonth.now() }
-    val startMonth = remember { currentMonth.minusMonths(100) } // Adjust as needed
-    val endMonth = remember { currentMonth.plusMonths(100) } // Adjust as needed
+    val startMonth = remember { currentMonth.minusMonths(500) } // Adjust as needed
+    val endMonth = remember { currentMonth.plusMonths(500) } // Adjust as needed
     val firstDayOfWeek = remember { firstDayOfWeekFromLocale() } // Available from the library
 
-    val daysOfWeek = daysOfWeek(firstDayOfWeek = DayOfWeek.SUNDAY)
+//    val daysOfWeek = daysOfWeek(firstDayOfWeek = DayOfWeek.SUNDAY)
+    val daysOfWeek = remember { daysOfWeek()}
+    var selection by remember { mutableStateOf<CalendarDay?>(null) }
 
     val state = rememberCalendarState(
         startMonth = startMonth,
@@ -86,22 +97,42 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     var currentDateText by remember { mutableStateOf("") }
     var currentSojuValue by remember { mutableIntStateOf(0) }
     var currentBeerValue by remember { mutableIntStateOf(0) }
-    rememberFirstVisibleWeekAfterScroll
+
+    val coroutineScope = rememberCoroutineScope()
+    val visibleMonth = rememberFirstCompletelyVisibleMonth(state)
+    LaunchedEffect(visibleMonth) {
+        selection = null
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text(getWeekPageTitle)
-        DaysOfWeekTitle(daysOfWeek = daysOfWeek)
+        SimpleCalendarTitle(
+            modifier = Modifier, currentMonth = visibleMonth.yearMonth,
+            goToPrevious = {
+                coroutineScope.launch {
+                    state.animateScrollToMonth(state.firstVisibleMonth.yearMonth.previousMonth)
+                    Log.println(Log.ASSERT, "SEGYEOL : ", " PREVIOUS SWIPED")
+                }
+            },
+            goToNext = {
+                coroutineScope.launch {
+                    state.animateScrollToMonth(state.firstVisibleMonth.yearMonth.nextMonth)
+                    Log.println(Log.ASSERT, "SEGYEOL : ", " NEXT SWIPED")
+                }
+            },
+        )
+//        DaysOfWeekTitle(daysOfWeek = daysOfWeek)
         HorizontalCalendar(
             state = state,
             dayContent = { day ->
                 Day(day, isSelected = selectedDate == day.date) { day ->
                     selectedDate = if (selectedDate == day.date) null else day.date
                     currentDateText = selectedDate.toString()
-                    Log.println(Log.ASSERT,"SEGYEOL : ",selectedDate.toString())
+                    Log.println(Log.ASSERT, "SEGYEOL : ", selectedDate.toString())
                 }
-            }
+            },
 //        dayContent = { Day(it) }
         )
         Spacer(modifier = modifier)
@@ -125,12 +156,58 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     }
 
 
-
 //    VerticalCalendar(
 //        state = state,
 //        dayContent = { Day(it) }
 //    )
 }
+
+@Composable
+fun rememberFirstCompletelyVisibleMonth(state: CalendarState): CalendarMonth {
+    val visibleMonth = remember(state) { mutableStateOf(state.firstVisibleMonth) }
+    Log.println(Log.ASSERT, "SEGYEOL : CURRENT=", visibleMonth.value.toString())
+    LaunchedEffect(state) {
+        snapshotFlow { state.layoutInfo.completelyVisibleMonths.firstOrNull() }
+            .filterNotNull()
+            .collect { month -> visibleMonth.value = month }
+    }
+    return visibleMonth.value
+}
+
+private val CalendarLayoutInfo.completelyVisibleMonths: List<CalendarMonth>
+    get() {
+        val visibleItemsInfo = this.visibleMonthsInfo.toMutableList()
+        return if (visibleItemsInfo.isEmpty()) {
+            emptyList()
+        } else {
+            val lastItem = visibleItemsInfo.last()
+            val viewportSize = this.viewportEndOffset + this.viewportStartOffset
+            if (lastItem.offset + lastItem.size > viewportSize) {
+                visibleItemsInfo.removeLast()
+            }
+            val firstItem = visibleItemsInfo.firstOrNull()
+            if (firstItem != null && firstItem.offset < this.viewportStartOffset) {
+                visibleItemsInfo.removeFirst()
+            }
+            visibleItemsInfo.map { it.month }
+        }
+    }
+
+private fun CalendarLayoutInfo.firstMostVisibleMonth(viewportPercent: Float = 50f): CalendarMonth? {
+    return if (visibleMonthsInfo.isEmpty()) {
+        null
+    } else {
+        val viewportSize = (viewportEndOffset + viewportStartOffset) * viewportPercent / 100f
+        visibleMonthsInfo.firstOrNull { itemInfo ->
+            if (itemInfo.offset < 0) {
+                itemInfo.offset + itemInfo.size >= viewportSize
+            } else {
+                itemInfo.size - itemInfo.offset >= viewportSize
+            }
+        }?.month
+    }
+}
+
 
 @Composable
 fun DaysOfWeekTitle(daysOfWeek: List<DayOfWeek>) {
